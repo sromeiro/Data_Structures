7bf6b337fd53d492ff8dbe577154386c112d608a
brettw894 <brettw1@mail.usf.edu>
2017-05-17 19:54:03 -0400

    added cout to test my github upload

--- Project_01/menu.cpp
+++ Project_01/menu.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
   cout << "\nQuick compile test" << endl;
-
+  cout << "Test if my github upload works\n" << endl;
   cout << "Succesfull program termination. GGEZ!\n" << endl;
   return 0;
 }
